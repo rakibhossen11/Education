88cf5ac64ced71ce768ad41cc743e3892ebd8e99
Rakib Hossen <rakibhossen1660@gmail.com>
2023-08-02 16:22:04 +0600

    create bottom navigation

---
+++ src/pages/Mytab.js
@@ -0,0 +1,20 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import Home from './Home';
+import Read from './Read';
+
+const Tab = createBottomTabNavigator();
+
+export class Mytab extends Component {
+  render() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Read' component={Read} />
+      </Tab.Navigator>
+    )
+  }
+}
+
+export default Mytab;
---
+++ src/pages/Read.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class Read extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Read </Text>
+      </View>
+    )
+  }
+}
+
+export default Read;
